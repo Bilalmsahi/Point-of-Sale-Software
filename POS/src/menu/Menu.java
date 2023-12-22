4ca5470c1f740187c46ca6cf8c96aefb8d5c04c7
Bilal Ahmed <154605440+Bilalmsahi@users.noreply.github.com>
2023-12-22 20:51:10 +0500

    Add files via upload

---
+++ POS/src/menu/Menu.java
@@ -0,0 +1,268 @@
+package raven.menu;
+
+import raven.menu.mode.LightDarkMode;
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.util.UIScale;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.LayoutManager;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import raven.application.form.*;
+
+/**
+ *
+ * @author BilalSahi
+ */
+public class Menu extends JPanel {
+    
+    
+    private final String menuItems[][] = {
+        {"~MAIN~"},
+        {"Home"},
+        {"Dashboard"},
+        {"Sales"},
+        {"Return"},
+        {"Customers", "Add New"},
+        {"Vouchers & Discounts"},
+        {"~MANAGEMENT~"},
+        {"Inventory", "Stock Available", "Add New Product", "Add New Product Category", "Add/Update Stock", "Low Stock", "Expired Stock"},
+        {"Staff Management"},
+        {"Store Expenses", "Add Expenses"},
+        {"Users", "Manage User", "Add User Role"},
+        {"Accounts","Balance View"},
+        {"~SUPPLIERS~"},
+        {"Supplier Record", "Suppliers", "Ledger"},
+        {"Orders Management", "Make an Order", "Status Management", "Payment Management","Supplier Payment History"},
+        {"Return TO Supplier"},
+        {"~REPORTS~"}, 
+        {"Reports","Available Stock", "Low Stock", "Sales", "Balance & Profits", "Total Stock Added", "Total Products Sold", "All Expenses Report", "Total Products Returned", "Returned To Supplier","Supplier Payment History"},
+        {"Back Up"},
+        {"Logout"},
+        {"About"},
+        {"Properties"}
+    };
+
+    public boolean isMenuFull() {
+        return menuFull;
+    }
+
+    public void setMenuFull(boolean menuFull) {
+        this.menuFull = menuFull;
+        if (menuFull) {
+            header.setText(headerName);
+            header.setHorizontalAlignment(JLabel.LEFT);
+        } else {
+            header.setText("");
+            header.setHorizontalAlignment(JLabel.CENTER);
+        }
+        for (Component com : panelMenu.getComponents()) {
+            if (com instanceof MenuItem) {
+                ((MenuItem) com).setFull(menuFull);
+            }
+        }
+        lightDarkMode.setMenuFull(menuFull);
+    }
+
+    private final List<MenuEvent> events = new ArrayList<>();
+    private boolean menuFull = true;
+    private final String headerName = "POS";
+
+    protected final boolean hideMenuTitleOnMinimum = true;
+    protected final int menuTitleLeftInset = 5;
+    protected final int menuTitleVgap = 5;
+    protected final int menuMaxWidth = 250;
+    protected final int menuMinWidth = 60;
+    protected final int headerFullHgap = 5;
+
+    public Menu() {
+        init();
+    }
+
+    private void init() {
+        setLayout(new MenuLayout());
+        putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:20,2,2,2;"
+                + "background:$Menu.background;"
+                + "arc:10");
+        header = new JLabel(headerName, new ImageIcon(getClass().getResource("/raven/icon/png/logo.png")), JLabel.LEFT);
+        header.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:$Menu.header.font;"
+                + "foreground:$Menu.foreground");
+
+        //  Menu
+        scroll = new JScrollPane();
+        panelMenu = new JPanel(new MenuItemLayout(this));
+        panelMenu.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5;"
+                + "background:$Menu.background");
+
+        scroll.setViewportView(panelMenu);
+        scroll.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:null");
+        JScrollBar vscroll = scroll.getVerticalScrollBar();
+        vscroll.setUnitIncrement(10);
+        vscroll.putClientProperty(FlatClientProperties.STYLE, ""
+                + "width:$Menu.scroll.width;"
+                + "trackInsets:$Menu.scroll.trackInsets;"
+                + "thumbInsets:$Menu.scroll.thumbInsets;"
+                + "background:$Menu.ScrollBar.background;"
+                + "thumb:$Menu.ScrollBar.thumb");
+        createMenu();
+        lightDarkMode = new LightDarkMode();
+        add(header);
+        add(scroll);
+        add(lightDarkMode);
+    }
+
+    private void createMenu() {
+        int index = 0;
+        for (int i = 0; i < menuItems.length; i++) {
+            String menuName = menuItems[i][0];
+            if (menuName.startsWith("~") && menuName.endsWith("~")) {
+                panelMenu.add(createTitle(menuName));
+            } else {
+                MenuItem menuItem = new MenuItem(this, menuItems[i], index++, events);
+                panelMenu.add(menuItem);
+            }
+        }
+    }
+
+    private JLabel createTitle(String title) {
+        String menuName = title.substring(1, title.length() - 1);
+        JLabel lbTitle = new JLabel(menuName);
+        lbTitle.putClientProperty(FlatClientProperties.STYLE, ""
+                + "foreground:$Menu.title.foreground");
+        return lbTitle;
+    }
+
+    public void setSelectedMenu(int index, int subIndex) {
+        runEvent(index, subIndex);
+    }
+
+    protected void setSelected(int index, int subIndex) {
+        int size = panelMenu.getComponentCount();
+        for (int i = 0; i < size; i++) {
+            Component com = panelMenu.getComponent(i);
+            if (com instanceof MenuItem) {
+                MenuItem item = (MenuItem) com;
+                if (item.getMenuIndex() == index) {
+                    item.setSelectedIndex(subIndex);
+                } else {
+                    item.setSelectedIndex(-1);
+                }
+            }
+        }
+    }
+
+    protected void runEvent(int index, int subIndex) {
+        MenuAction menuAction = new MenuAction();
+        for (MenuEvent event : events) {
+            event.menuSelected(index, subIndex, menuAction);
+        }
+        if (!menuAction.isCancel()) {
+            setSelected(index, subIndex);
+        }
+    }
+
+    public void addMenuEvent(MenuEvent event) {
+        events.add(event);
+    }
+
+    public void hideMenuItem() {
+        for (Component com : panelMenu.getComponents()) {
+            if (com instanceof MenuItem) {
+                ((MenuItem) com).hideMenuItem();
+            }
+        }
+        revalidate();
+    }
+
+    public boolean isHideMenuTitleOnMinimum() {
+        return hideMenuTitleOnMinimum;
+    }
+
+    public int getMenuTitleLeftInset() {
+        return menuTitleLeftInset;
+    }
+
+    public int getMenuTitleVgap() {
+        return menuTitleVgap;
+    }
+
+    public int getMenuMaxWidth() {
+        return menuMaxWidth;
+    }
+
+    public int getMenuMinWidth() {
+        return menuMinWidth;
+    }
+
+    private JLabel header;
+    private JScrollPane scroll;
+    private JPanel panelMenu;
+    private LightDarkMode lightDarkMode;
+
+    private class MenuLayout implements LayoutManager {
+
+        @Override
+        public void addLayoutComponent(String name, Component comp) {
+        }
+
+        @Override
+        public void removeLayoutComponent(Component comp) {
+        }
+
+        @Override
+        public Dimension preferredLayoutSize(Container parent) {
+            synchronized (parent.getTreeLock()) {
+                return new Dimension(5, 5);
+            }
+        }
+
+        @Override
+        public Dimension minimumLayoutSize(Container parent) {
+            synchronized (parent.getTreeLock()) {
+                return new Dimension(0, 0);
+            }
+        }
+
+        @Override
+        public void layoutContainer(Container parent) {
+            synchronized (parent.getTreeLock()) {
+                Insets insets = parent.getInsets();
+                int x = insets.left;
+                int y = insets.top;
+                int gap = UIScale.scale(5);
+                int sheaderFullHgap = UIScale.scale(headerFullHgap);
+                int width = parent.getWidth() - (insets.left + insets.right);
+                int height = parent.getHeight() - (insets.top + insets.bottom);
+                int iconWidth = width;
+                int iconHeight = header.getPreferredSize().height;
+                int hgap = menuFull ? sheaderFullHgap : 0;
+                header.setBounds(x + hgap, y, iconWidth - (hgap * 2), iconHeight);
+
+                int ldgap = 10;
+                int ldWidth = width - ldgap * 2;
+                int ldHeight = lightDarkMode.getPreferredSize().height;
+                int ldx = x + ldgap;
+                int ldy = y + height - ldHeight - ldgap;
+
+                int menux = x;
+                int menuy = y + iconHeight + gap;
+                int menuWidth = width;
+                int menuHeight = height - (iconHeight + gap) - (ldHeight + ldgap * 2);
+                scroll.setBounds(menux, menuy, menuWidth, menuHeight);
+
+                lightDarkMode.setBounds(ldx, ldy, ldWidth, ldHeight);
+            }
+        }
+    }
+}
