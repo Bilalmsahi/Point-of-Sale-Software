c582716683f5cc22a0a1ab03f9b04ef0f5bf56a2
Bilal Ahmed <154605440+Bilalmsahi@users.noreply.github.com>
2023-12-22 20:46:34 +0500

    Add files via upload

---
+++ POS/src/application/Application.java
@@ -0,0 +1,91 @@
+package raven.application;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.extras.FlatAnimatedLafChange;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import raven.application.form.LoginForm;
+import raven.application.form.MainForm;
+import raven.toast.Notifications;
+import raven.toast.ToastClientProperties;
+
+/**
+ *
+ * @author Raven
+ */
+public class Application extends javax.swing.JFrame {
+
+    private static Application app;
+    public final MainForm mainForm;
+    private final LoginForm loginForm;
+
+    public Application() {
+        initComponents();
+        setSize(new Dimension(1200, 768));
+        setLocationRelativeTo(null);
+        mainForm = new MainForm();
+        loginForm = new LoginForm();
+        setContentPane(loginForm);
+        Notifications.getInstance().setJFrame(this);
+    }
+
+    public static void showForm(Component component) {
+        app.mainForm.showForm(component);
+    }
+
+    public static void login() {
+        FlatAnimatedLafChange.showSnapshot();
+        app.setContentPane(app.mainForm);
+        setSelectedMenu(0, 0);
+        app.mainForm.hideMenu();
+        SwingUtilities.updateComponentTreeUI(app.mainForm);
+        FlatAnimatedLafChange.hideSnapshotWithAnimation();
+    }
+
+    public static void logout() {
+        FlatAnimatedLafChange.showSnapshot();
+        app.setContentPane(app.loginForm);
+        SwingUtilities.updateComponentTreeUI(app.loginForm);
+        FlatAnimatedLafChange.hideSnapshotWithAnimation();
+    }
+
+    public static void setSelectedMenu(int index, int subIndex) {
+        app.mainForm.setSelectedMenu(index, subIndex);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 907, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 650, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        FlatLaf.registerCustomDefaultsSource("raven.theme");
+        FlatDarculaLaf.setup();
+        java.awt.EventQueue.invokeLater(() -> {
+            app = new Application();
+            app.setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
